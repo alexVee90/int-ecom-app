31f5ff0c6181bb67d87781e29849ba7111d8da46
alex <alexandrucristianvisan@gmail.com>
2020-05-09 11:33:07 +0300

    fixed-active-navtags

--- util/navtagsHandler.js
+++ util/navtagsHandler.js
@@ -2,13 +2,13 @@
   @desc adds to every view two boolean values
 */
 module.exports = (req, res, next) => { 
-  let mens, womens;
+  let womens, genderSelected;
   if(req.originalUrl !== '/favicon.ico') { 
     const urlArr = req.originalUrl.split('/');
-    mens = urlArr.includes('mens');
-    womens = urlArr.includes('womens');
+    if(urlArr[1] === '' || urlArr[1] === 'products') genderSelected = true;
+    urlArr.forEach(param => { if(param.includes('womens')) womens = true });
   }
-  res.locals.mens = mens;
   res.locals.womens = womens;
+  res.locals.genderSelected = genderSelected;
   next();
 }
