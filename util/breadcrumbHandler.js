25f7996a430079a338b4baa39f29fa078b753139
alex <alexandrucristianvisan@gmail.com>
2020-05-10 13:34:45 +0300

    fixed-breadcrumbs

--- util/breadcrumbHandler.js
+++ util/breadcrumbHandler.js
@@ -5,7 +5,17 @@
 module.exports = (req, res, next) => { 
   let routes = [];
   if(req.originalUrl !== '/favicon.ico') {
-    urlArr = req.originalUrl.split('/').slice(1); // @returns ex: ['', 'home', 'category'].slice(1)
+    let urlArr = req.originalUrl.split('/').slice(1); // @returns ex: ['', 'home', 'category'].slice(1)
+
+    const regex = /\?page\=[0-9][0-9]?/;
+    
+    /*
+      tests if the last parameter in the route is the page query parameter and removes it
+    */
+    if(regex.test(urlArr[urlArr.length - 1])) {
+      urlArr = urlArr.slice(0, urlArr.length - 1)
+    }
+
     routes = urlArr.map((r, i, arr) => {
       return { name: r, route: arr.slice(0, i + 1).join('/')}  //@returns an array of objects with name of the route and the route;
     })
--- util/navtagsHandler.js
+++ util/navtagsHandler.js
@@ -2,22 +2,22 @@
   @desc adds to every view two boolean values
 */
 module.exports = (req, res, next) => { 
-  let womens, mens, genderSelected;
+  let womens, mens;
   if(req.originalUrl !== '/favicon.ico') { 
-    let temp = req.originalUrl;
-    if(temp.substr(-1) === '/') temp = temp.substr(0, temp.length - 1); //removes the final slash from the url string
+    /*
+      removes the final slash from req.originalUrl
+    */
+    let temp = (req.originalUrl.substr(-1) === '/') ? req.originalUrl.substr(0, req.originalUrl.length - 1) : req.originalUrl; 
 
     const urlArr = temp.split('/');    // creates an array from the url
 
     const m = /^mens$/;
     const w = /^womens$/;
 
-    // if(urlArr[urlArr.length - 1] === '' || urlArr[urlArr.length - 1] === 'products' || urlArr[urlArr.length - 1] === 'categories') genderSelected = true;
     urlArr.forEach(param => { if(w.test(param)) womens = true });
     urlArr.forEach(param => { if(m.test(param)) mens = true });
   }
   res.locals.womens = womens;
-  res.locals.genderSelected = genderSelected;
   res.locals.mens = mens;
   next();
 }
