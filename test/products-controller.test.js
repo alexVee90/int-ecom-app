309068e807fecd26100767b9a6d721ee0d2bab4a
alex <alexandrucristianvisan@gmail.com>
2020-05-12 00:41:51 +0300

    tested-getProducts-handler

--- test/products-controller.test.js
+++ test/products-controller.test.js
@@ -1,20 +1,50 @@
 const { expect } = require('chai');
-const { getProduct } = require('../controllers/products');
+const { getProduct, getProducts } = require('../controllers/products');
 
 describe('Products Controller', function() { 
-  it('should throw an error if productId is invalid', function(done) { 
-    const req = { 
-      params: {
-        id: 1,
-        categoryId: 'mens'
-      }
-      
-    };
-   
-    getProduct(req, {}, () => {}).then(result => {
-      expect(result).to.be.an('error');
-      done();
+  describe('Get Product Handler', function() {
+    it('should throw an error if id is invalid', function(done) { 
+      const req = { 
+        params: {
+          id: 1,
+          categoryId: 'mens'
+        }
+        
+      };
+      getProduct(req, {}, () => {}).then(result => {
+        expect(result).to.be.an('error');
+        done();
+      })
     })
-
+  
+    it('should throw an error if categoryId is invalid', function(done) { 
+      const req = { 
+        params: {
+          id: 1,
+          categoryId: 'masdasdaens'
+        }
+        
+      };
+      getProduct(req, {}, () => {}).then(result => {
+        expect(result).to.be.an('error');
+        done();
+      })
+   })
   })
+
+  describe('Get Products Handler', function() {
+    it('should throw an error if categoryId is invalid', function(done) { 
+      const req = { 
+        params: {
+          categoryId: 'masdasdsdfaens'
+        }
+        
+      };
+      getProducts(req, {}, () => {}).then(result => {
+        expect(result).to.be.an('error');
+        done();
+      })
+   })
+  })
+
 })
