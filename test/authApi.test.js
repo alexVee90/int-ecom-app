9aea2260ca04358ab7ea000dd2b2d2075a1d4f81
alex <alexandrucristianvisan@gmail.com>
2020-05-14 23:37:34 +0300

    tested-auth-wishlist-api

--- test/authApi.test.js
+++ test/authApi.test.js
@@ -2,9 +2,13 @@ const { expect } = require('chai');
 const {
   signUp,
   signIn,
+  addToWishlist,
+  getWishlistFromDB,
+  deleteWishlistItem,
+  changeWishlistItemQuantity
 } = require('../models/authApi');
 
-describe('Auth Api', function() {
+describe('Auth Api', function() { 
 
   describe('Sign in Function', function() { 
     const data = {
@@ -29,7 +33,6 @@ describe('Auth Api', function() {
   });
 
   describe('Sign Up Function', function() { 
-
     const invalid = {
       email: 'test1@gmail.com',
     };
@@ -39,6 +42,49 @@ describe('Auth Api', function() {
         expect(err).to.be.instanceOf(Error);
         done();
       });
+    });
+  });
+
+  describe('Add to WishList Function', function() { 
+    const invalidToken = 'asdasdasdadasd';
+    const data = {}
+    it('should return an Error if token is invalid', done => {
+      addToWishlist(data, invalidToken).catch(err => {
+        expect(err).to.be.an.instanceOf(Error);
+        done();
+      })
+    });
+  });
+
+  describe('GET WishList Function', function() { 
+    const invalidToken = 'asdasdasdadasd';
+    it('should return an Error if token is invalid', done => {
+      getWishlistFromDB(invalidToken).catch(err => {
+        expect(err).to.be.an.instanceOf(Error);
+        done();
+      })
+    });
+  });
+
+  describe('Delete Wishlist item Function', function() { 
+    const invalidToken = 'asdasdasdadasd';
+    const data = {}
+    it('should return an Error if token is invalid', done => {
+      deleteWishlistItem(data, invalidToken).catch(err => {
+        expect(err).to.be.an.instanceOf(Error);
+        done();
+      })
+    });
+  });
+
+  describe('Change Wishlist item Quantity Function', function() { 
+    const invalidToken = 'asdasdasdadasd';
+    const data = {}
+    it('should return an Error if token is invalid', done => {
+      changeWishlistItemQuantity(data, invalidToken).catch(err => {
+        expect(err).to.be.an.instanceOf(Error);
+        done();
+      })
     });
   });
 
