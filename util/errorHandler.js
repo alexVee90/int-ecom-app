227321228be85a73ce56684a9cf7eec9985c13c4
alex <alexandrucristianvisan@gmail.com>
2020-05-23 15:51:04 +0300

    removed-winston

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,20 +1,4 @@
 
-
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log` 
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-  ]
-});
-
 /*
   @desc error middleware, adds a default statuscode and message if these are undefined. 
   @returns error ejs template
@@ -38,24 +22,12 @@ module.exports = (err, req, res, next) => {
   if(err.name === 'ValidationError') {
     err.status = 400;
     err.msg = ` The following fields are required: ${Object.keys(err.errors)}`;
-    logger.log({
-      level: 'error',
-      msg: err 
-    });
     return res.status(err.status).send({ success: false, reason: err.msg, data: null });
   }
   if(err.name === 'CastError') {
     err.status = 400;
     err.msg = err.message;
-    logger.log({
-      level: 'error',
-      msg: err 
-    });
     return res.status(err.status).send({ success: false, reason: err.msg, data: null });
   }
-  logger.log({
-    level: 'error',
-    msg: err 
-  });
   res.render('error', { err });
 }
